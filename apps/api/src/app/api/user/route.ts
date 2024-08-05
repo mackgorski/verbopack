d96315d5f29a624c45aa4166e2d9c4bca554d755
Mack Górski <office@mgorski.com>
2024-08-05 22:51:56 +0200

    fix: Handle errors in user route

--- apps/api/src/app/api/user/route.ts
+++ apps/api/src/app/api/user/route.ts
@@ -109,19 +109,24 @@ import { NextResponse } from 'next/server';
 import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
 import prisma from '../../../lib/prisma';
 
-export const GET = withApiAuthRequired(async function route() {
-    const session = await getSession();
-    if (!session || !session.user) {
-        return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+export const GET = withApiAuthRequired(async function route(req) {
+    try {
+        const session = await getSession(req);
+        if (!session || !session.user) {
+            return NextResponse.json({ error: 'Not authenticated' }, { status: 401 });
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { auth0Id: session.user.sub },
+        });
+
+        if (!user) {
+            return NextResponse.json({ error: 'User not found' }, { status: 404 });
+        }
+
+        return NextResponse.json(user);
+    } catch (error) {
+        console.error('Error in user route:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
     }
-
-    const user = await prisma.user.findUnique({
-        where: { auth0Id: session.user.sub },
-    });
-
-    if (!user) {
-        return NextResponse.json({ error: 'User not found' }, { status: 404 });
-    }
-
-    return NextResponse.json(user);
 });
